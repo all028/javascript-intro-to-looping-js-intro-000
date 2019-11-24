c27e236b3e1f02b623ab4831736af8ce5f367722
al <albliu@email.com>
2019-11-24 00:48:29 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,22 @@
+function forLoop(arr){
+  //for (int i=0; i <=24; i++){
+//     arr[x];
+//  }
+
+}
+
+function whileLoop(num){
+  while (num > 0) {
+    console.log(--num);
+  }
+   return 'done';
+}
+
+function doWhileLoop(num){
+  var x =1
+  do {
+    console.log('I run once regardless');
+    x++;
+  } while (x<=num);
+
+}
